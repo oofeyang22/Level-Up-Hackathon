10dfeb756742b8a0b63387838c319fa9ff6cb449
Olaitan Feranmi <olaitanferanmi41@gmail.com>
2023-12-24 12:08:23 -0800

    3rd commit

--- index.js
+++ index.js
@@ -53,3 +53,41 @@ ellipses.forEach(function (ellipse) {
         ellipse.src= "CircleTickMajor.png";
     })
 })
+let changer= document.querySelector(".changer")
+let indicator= document.querySelector(".indicator")
+let progressbar= document.querySelector(".progressbar")
+let currentStep= 1;
+
+
+let first= document.querySelector("#first");
+
+let second = document.querySelector("#second");
+
+let third= document.querySelector("#third");
+
+let fourth = document.querySelector("#fourth");
+let fifth= document.querySelector("#fifth");
+
+first.addEventListener("click", () => {
+  indicator.style.width= `20%`;
+  changer.innerHTML= "1"
+})
+
+second.addEventListener("click", () => {
+  indicator.style.width= `40%`;
+  changer.innerHTML= "2"
+})
+
+third.addEventListener("click", () => {
+  indicator.style.width= `60%`;
+  changer.innerHTML= "3"
+})
+
+fourth.addEventListener("click", () => {
+  indicator.style.width= `80%`;
+  changer.innerHTML= "4"
+})
+fifth.addEventListener("click", () => {
+  indicator.style.width= `100%`;
+  changer.innerHTML= "5"
+})
